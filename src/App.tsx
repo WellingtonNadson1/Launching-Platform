991dc869068524029ae38423cce24821b6d8f9bd
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-06-21 13:33:59 -0300

    Get data with GraphQl in the GraphCMS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lessons {
+  id: string;
+  title: string;
+}
+
+
 
 function App() {
+  const { data } = useQuery< { lessons: Lessons[] } >(GET_LESSONS_QUERY);
+
 
   return (
-    <h1 className="text-violet-700">Wellington Nadson</h1>
+    <ul>
+      {data?.lessons.map((lesson) => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
+
 }
 
 export default App
