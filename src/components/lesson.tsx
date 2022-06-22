79c341dfe0b0236a4a04d5037cc837651c10e037
WellingtonNadson1 <wellingtonnadson@gmail.com>
2022-06-21 23:57:47 -0300

    Created sidebar component

--- src/components/lesson.tsx
+++ src/components/lesson.tsx
@@ -1,24 +1,46 @@
-import { CheckCircle } from "phosphor-react";
+import { CheckCircle, Lock } from "phosphor-react";
+import { isPast, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
-export function Lesson(){
+interface LessonPropos {
+    title: string;
+    slug: string;
+    availableAt: Date;
+    type: 'live' | 'class';
+}
+
+export function Lesson(props: LessonPropos){
+
+    const isavailableAt = isPast(props.availableAt)
+    const availableDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm'm'", {locale: ptBR})
+
     return (
         <a href="#">
             <span>
-                Terça 22 de junho 19h00
+                {availableDateFormated}
             </span>
 
             <div className="border border-gray-500 rounded p-4 mt-2">
                 <header className="flex items-center justify-between">
-                    <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-                        <CheckCircle size={20}/>
-                        Conteúdo Liberado
-                    </span>
+                    
+                    {isavailableAt ? (
+                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                            <CheckCircle size={20}/>
+                            Conteúdo Liberado
+                        </span>
+                    ) : (
+                        <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
+                            <Lock size={20}/>
+                            EM breve
+                        </span>
+                    )}
+
                     <span className="text-xs rounded px-2 py-[0.125rem] border border-green-300 font-bold">
-                        AO VIVO
+                        {props.type === 'live' ? "AO VIVO" : 'AULA PRÁTICA'}
                     </span>
                 </header>
                 <strong className="text-gray-200 mt-5 block">
-                    Abertura do evento Ignite Lab
+                    {props.title}
                 </strong>
             </div>
         </a>
